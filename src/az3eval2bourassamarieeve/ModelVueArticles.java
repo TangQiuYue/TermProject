6fbec604620deeb932b2f78a3c9bbebc086b300d
TangQiuYue <43708759+TangQiuYue@users.noreply.github.com>
2019-06-02 21:11:05 -0400

    Completed ModelVueArticles
    All methodes completed.

--- src/az3eval2bourassamarieeve/ModelVueArticles.java
+++ src/az3eval2bourassamarieeve/ModelVueArticles.java
@@ -7,22 +7,133 @@ package az3eval2bourassamarieeve;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author TQY_06
  */
 public class ModelVueArticles {
-    
-    
-    public Connection connectToBD(){
-        Connection connect = null;
-       try{ connect = DriverManager.getConnection("jdbc:derby://localhost:1527/Az3Eval2Bourassa", "username", "password");
-        System.out.println("Connexion Obtenue");
-       } catch(SQLException e) {
-           e.printStackTrace();
-       }
+
+    private static String url = "jdbc:derby://localhost:1527/Az3Eval2Bourassa";
+    private static String user = "username";
+    private static String passwd = "password";
+
+    private Connection connect = null;
+    private String sqlString;
+    private Statement stmt;
+    private ResultSet resSet;
+
+    public Connection connectToBD() {
+        try {
+            connect = DriverManager.getConnection(url, user, passwd);
+            System.out.println("Connexion Obtenue");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return connect;
     }
+
+    public void getArticles() {
+        try {
+            connect = DriverManager.getConnection(url, user, passwd);
+            stmt = connect.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            sqlString = "Select * from ARTICLES";
+            resSet = stmt.executeQuery(sqlString);
+            while (resSet.next()) {
+                System.out.println(resSet.getString(2));
+            }
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                resSet.close();
+                stmt.close();
+                connect.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(ModelVueArticles.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+        }
+    }
+        public void modifyArticles(String  codesArticles, String designationArticles, int codeCategories, double prixUnitaire) {
+        try {
+            connect = DriverManager.getConnection(url, user, passwd);
+            stmt = connect.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            sqlString = "Select * from Etudiant where numetudiant = " + codesArticles;
+            resSet = stmt.executeQuery(sqlString);
+            resSet.next();
+            resSet.updateString(2, designationArticles);
+            resSet.updateInt(3, codeCategories);
+            resSet.updateDouble(4, prixUnitaire);
+            resSet.updateRow();
+            System.out.println("Article successfully modified");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                resSet.close();
+                stmt.close();
+                connect.close();
+            } catch (Exception ex) {
+                Logger.getLogger(ModelVueArticles.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    
+    
+     public void deleteArticles(String codeArticles) {
+        try {
+            connect = DriverManager.getConnection(url, user, passwd);
+            stmt = connect.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            sqlString = "Select * from Articles where CodeArticles = " + codeArticles;
+            resSet = stmt.executeQuery(sqlString);
+            resSet.next();
+            resSet.deleteRow();
+            System.out.println("You have properly deleted this article from the database");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                resSet.close();
+                stmt.close();
+                connect.close();
+            } catch (Exception ex) {
+                Logger.getLogger(ModelVueArticles.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    
+    public void addArticles(String  codesArticles, String designationArticles, int codeCategories, double prixUnitaire) {
+        try {
+            connect = DriverManager.getConnection(url, user, passwd);
+            stmt = connect.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            sqlString = "Select * from Articles ";
+            resSet = stmt.executeQuery(sqlString);
+            resSet.moveToInsertRow();
+            resSet.updateString(1, codesArticles);
+            resSet.updateString(2, designationArticles);
+            resSet.updateInt(3, codeCategories);
+            resSet.updateDouble(4, prixUnitaire);
+            resSet.insertRow();
+            System.out.println("New Article inserted into Database Successfully");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                resSet.close();
+                stmt.close();
+                connect.close();
+            } catch (Exception ex) {
+                Logger.getLogger(ModelVueArticles.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    
+    
 }
